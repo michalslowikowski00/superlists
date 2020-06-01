c221e512d6eabc37048335edc97c19348fbd5619
michalslowikowski00 <michal.slowikowski@polidea.com>
2020-06-01 15:18:00 +0200

    Added new asserions in func test

--- func_test.py
+++ func_test.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 import unittest
 
+from selenium.webdriver.common.keys import Keys
+
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -14,16 +16,28 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edyta dowiedziała się o nowej, wspaniałej aplikacji w postaci listy rzeczy do zrobienia.
         # Postanowiła więc przejść na stronę główną tej aplikacji.
-
         self.browser.get('http://127.0.0.1:8000')
-        # Zwróciła uwagę, że tytuł strony i nagłówek zawierają słowo Listy.
 
-        self.assertIn('Listy', self.browser.title)
-        self.fail('Zakończenie testu!')
+        # Zwróciła uwagę, że tytuł strony i nagłówek zawierają słowo Listy.
+        self.assertIn('Lista', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Lista', header_text)
+
         # Od razu zostaje zachęcona, aby wpisać rzecz do zrobienia.
-        # W polu tekstowym wpisała "Kupić pawie pióra" (hobby Edyty polega na tworzeniu ozdobnych przynęt).
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Wpisz rzeczy do zrobienia')
+
+        # W polu tekstowym wpisała "Kupić pawie pióra"
+        # (hobby Edyty polega na tworzeniu ozdobnych przynęt).
+        inputbox.send_keys('Kupic pawie piora')
+
         # Po naciśnięciu klawisza Enter strona została uaktualniona i wyświetla
         # "1: Kupić pawie pióra" jako element listy rzeczy do zrobienia.
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Kupic pawie piora' for row in rows))
+
         # Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadania.
         # Edyta wpisała "Użyć pawich piór do zrobienia przynęty" (Edyta jest niezwykle skrupulatna).
         # Strona została ponownie uaktualniona i teraz wyświetla dwa elementy na liście rzeczy do zrobienia.
@@ -32,6 +46,8 @@ class NewVisitorTest(unittest.TestCase):
         # obok którego znajduje się pewien tekst z wyjaśnieniem
         # Przechodzi pod podany adres URL i widzi wyświetloną swoją listę rzeczy do zrobienia.
         # Usatysfakcjonowana kładzie się spać.
+
+        self.fail('Zakończenie testu!')
 
 
 if __name__ == '__main__':
