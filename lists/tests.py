d14e3965afd275ce2e90c7c54adfed06b602cdd5
michalslowikowski00 <michal.slowikowski@polidea.com>
2020-06-01 14:16:56 +0200

    Simple view with minimal html

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
@@ -6,6 +7,16 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
+    # def home_page(request):
+    #     pass
+
     def test_root_url_resolves_to_home_page(self):
         found = resolve('/')
         self.assertEquals(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Lista rzeczy do zrobienia</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 # from superlists.lists.views import home_page
 
 
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>Lista rzeczy do zrobienia</title></html>')
